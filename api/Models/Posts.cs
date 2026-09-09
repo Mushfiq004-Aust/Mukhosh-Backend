4dee3246afab1ca72fc27e4529b77ef5b8cb41f9
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-09 23:37:07 +0600

    Update Posts.cs

--- api/Models/Posts.cs
+++ api/Models/Posts.cs
@@ -7,24 +7,24 @@ namespace api.Models
 {
     public class Posts
     {
-        public Guid postId { get; set; } // must provide value, cannot be null
+        public Guid PostsId { get; set; } // must provide value, cannot be null, must match the classname 
 
-        public required string title { get; set; } // must provide value, cannot be null
+        public required string Title { get; set; } // must provide value, cannot be null
 
-        public required string content { get; set; } // must provide value, cannot be null
+        public required string Content { get; set; } // must provide value, cannot be null
 
-        public DateTime createdAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
+        public DateTime CreatedAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
 
         //Empty List is a c# Object, so it will not be null, but it can be empty.
         //EF migration will create a foreign key relationship between the posts and comments tables.
         //Also i can use post.comments to get all the comments of a post.
-        public List<Comment> comments { get; set; } = new List<Comment>(); 
+        public List<Comment> Comments { get; set; } = new List<Comment>(); 
 
         //foreign key relationship between the posts and users tables.
-        public Guid userId { get; set; } // must provide value, cannot be null
+        public Guid UserId { get; set; } // must provide value, cannot be null
 
         //navigation property to the user who created the post.Through post.user, I can access the user who created the post.
-        public User user { get; set; } = null!; // to avoid null reference exception
+        public User User { get; set; } = null!; // to avoid null reference exception
         
     }
 }
