c5b8af1cff13b8bd0ef7fecf31bd801c3f5fdf9c
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-09 23:37:09 +0600

    Update Comment.cs

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -7,17 +7,17 @@ namespace api.Models
 {
     public class Comment
     {
-        public Guid commentId { get; set; } // must provide value, cannot be null
+        public Guid CommentId { get; set; } // must provide value, cannot be null, must match the classname 
 
-        public required string content { get; set; } // must provide value, cannot be null
+        public required string Content { get; set; } // must provide value, cannot be null
 
-        public DateTime createdAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
+        public DateTime CreatedAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
 
         //foreign key relationship between the comments and posts tables.
-        public Guid postId { get; set; } // must provide value, cannot be null
+        public Guid PostId { get; set; } // must provide value, cannot be null
 
         //navigation property to the post that the comment belongs to.
         //I can get the post of a comment through comment.post, and I can get the user of a comment through comment.post.user.
-        public Posts post { get; set; } = null!; // to avoid null reference exception
+        public Posts Posts { get; set; } = null!; // to avoid null reference exception
     }
 }
