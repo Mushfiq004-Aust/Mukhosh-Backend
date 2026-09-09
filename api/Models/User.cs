e39a511095f3f22a819500592f89d85cdc120b8b
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-09 23:37:05 +0600

    Update User.cs

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -7,20 +7,20 @@ namespace api.Models
 {
     public class User
     {
-        public Guid userId { get; set; } // must provide value, cannot be null
+        public Guid UserId { get; set; } // must provide value, cannot be null, must match the classname 
 
-        public required string name { get; set; } // must provide value, cannot be null
+        public required string Name { get; set; } // must provide value, cannot be null
 
-        public required string  email { get; set; } // must be unique, cannot be null
+        public required string Email { get; set; } // must be unique, cannot be null
 
         public string? Institution { get; set; } // If not provided, database get null value
 
-        public DateTime createdAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
-        
-        public string phone { get; set; } = string.Empty; // If not provided, database get empty string value
+        public DateTime CreatedAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
+
+        public string Phone { get; set; } = string.Empty; // If not provided, database get empty string value
 
         //Ef migration will create a foreign key relationship between the users and posts tables.
         //Also i can use user.posts to get all the posts of a user, and user.posts[x].comments to get all the comments of that post.
-        public List<Posts> posts { get; set; } = new List<Posts>();
+        public List<Posts> Posts { get; set; } = new List<Posts>();
     }
 }
