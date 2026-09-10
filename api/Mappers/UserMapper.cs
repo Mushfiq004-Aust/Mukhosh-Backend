3687b8de2addb522e2531d8dab90e30ea2430cc6
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-10 13:16:59 +0600

    Create UserMapper.cs

---
+++ api/Mappers/UserMapper.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.User;
+using api.Models;
+
+//Mapper is a class that maps the data from the database model to the response model, so that the response model can be sent to the client.
+namespace api.Mappers
+{
+    // its an extension class, so it should be static, because it will not be instantiated, but its methods will be called directly on the class itself.
+    public static class UserMapper
+    {
+        public static UserInView ToUserInView(this User user) // response model to the user
+        {
+            return new UserInView
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Institution = user.Institution,
+                Phone = user.Phone,
+                CreatedAt = user.CreatedAt
+            };
+        }
+
+        public static User ToUserInCreate(this UserInCreate user) // response model to the user
+        {
+            return new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                Institution = user.Institution,
+                Phone = user.Phone
+            };
+        }
+    }
+}
