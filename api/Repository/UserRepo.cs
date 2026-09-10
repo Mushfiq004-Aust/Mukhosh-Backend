f80ecbc84c71188cbd13601ea08482ed2fd11da1
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-10 20:54:11 +0600

    Update UserRepo.cs

--- api/Repository/UserRepo.cs
+++ api/Repository/UserRepo.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Database;
+using api.DTOs.User;
+using api.Mappers;
 using api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 // In the UserRepo class, we are implementing the IUserRepository interface and providing the implementation methods of the interface
@@ -18,9 +21,52 @@ namespace api.Repository
             _context = context;
         }
 
-        public Task<List<User>> GetAllUsers()
+        public async Task<List<User>> GetAllUsersAsync()
         {
-            return _context.User.ToListAsync();
+            return await _context.User.ToListAsync();
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid id)
+        {
+             return await _context.User.FindAsync(id);
+        }
+
+        public async Task<User> CreateUserAsync(UserInCreate user)
+        {
+            var newUser = user.ToUserInCreate(); //map the request body to the User model using the mapper class
+            await _context.User.AddAsync(newUser); // add the user to the database context
+            await _context.SaveChangesAsync(); // save the changes to the database
+            return newUser;
+        }
+
+        public async Task<User?> UpdateUserAsync(Guid id, UserInUpdate user)
+        {
+            var existingUser = await _context.User.FindAsync(id);
+            if (existingUser == null)
+            {
+                return null;
+            }
+
+            existingUser.Name = user.Name;
+            existingUser.Phone = user.Phone;
+
+            await _context.SaveChangesAsync();
+            return existingUser;
+        }
+
+        public async Task<User?> DeleteUserAsync(Guid id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return null;
+            }
+
+             _context.User.Remove(user);
+            // remove is not async, because it only marks the entity for deletion, and does not actually delete it from the database until SaveChangesAsync is called.
+            await _context.SaveChangesAsync();
+
+            return user; // returning the user instead of null
         }
     }
 }
