b1de97ed83d5910f82dee98c93179b6468bd82ba
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-10 13:17:03 +0600

    Create UserInView.cs

---
+++ api/DTOs/User/UserInView.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.DTOs.User
+{
+    public class UserInView
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Institution { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
