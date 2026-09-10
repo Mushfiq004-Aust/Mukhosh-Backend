59e6853aeb38a2a8ed899e999435ccb09d786a3d
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-11 00:35:17 +0600

    Create ICommentRepository.cs

---
+++ api/Interfaces/ICommentRepository.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.Comment;
+using api.Models;
+
+//Interface for Comment Repository
+//Here we define the contract for the Comment Repository
+//specifying the methods that any implementation of this interface must provide.
+//This allows for flexibility and easier testing, as we can swap out different implementations of ICommentRepository
+//without changing the code that depends on it.
+
+namespace api.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<List<Comment>> GetAllCommentsAsync();
+        Task<Comment?> GetCommentByIdAsync(Guid commentId);
+        Task<Comment> CreateCommentAsync(CommentInCreate comment);
+        Task<Comment?> UpdateCommentAsync(Guid commentId, CommentInUpdate comment);
+        Task<Comment?> DeleteCommentAsync(Guid commentId);
+    }
+}
