102215f7274b20714eedfce25f7c379c8e590975
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-11 00:35:23 +0600

    Create CommentMapper.cs

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentInView ToCommentView(this Comment comment)
+        {
+            return new CommentInView
+            {
+                Content = comment.Content,
+                CreatedAt = comment.CreatedAt
+            };
+        }
+
+        public static Comment ToCommentCreate(this CommentInCreate commentInCreate)
+        {
+            return new Comment
+            {
+                Content = commentInCreate.Content,
+                PostId = commentInCreate.PostId
+            };
+        }
+
+        public static Comment ToCommentUpdate(this CommentInUpdate commentInUpdate)
+        {
+            return new Comment
+            {
+                Content = commentInUpdate.Content
+            };
+        }
+    }
+}
