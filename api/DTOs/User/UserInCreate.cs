855ee2f105b65abe7e32556447ab48f6c631e044
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-10 16:32:35 +0600

    Update UserInCreate.cs
    Email validation

--- api/DTOs/User/UserInCreate.cs
+++ api/DTOs/User/UserInCreate.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.DTOs.User
 {
     public class UserInCreate
     {
         public required string Name { get; set; }
+
+        [EmailAddress] // for validating the email format
         public required string Email { get; set; }
         public required string Password { get; set; }
         public required string Institution { get; set; }
