fd9a837f84d5c31ccaf7c67565614d1cc4b72843
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-10 13:16:36 +0600

    Update User.cs
    added password field

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -15,11 +15,13 @@ namespace api.Models
         [EmailAddress] // for validating the email format
         public required string Email { get; set; } // must be unique, cannot be null
 
+        public required string Password { get; set; } // must provide value, cannot be null
+
         public string? Institution { get; set; } // If not provided, database get null value
 
-        public DateTime CreatedAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
-
         public string Phone { get; set; } = string.Empty; // If not provided, database get empty string value
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now; // If not provided, database get current date and time value
 
         //Ef migration will create a foreign key relationship between the users and posts tables.
         //Also i can use user.posts to get all the posts of a user, and user.posts[x].comments to get all the comments of that post.
