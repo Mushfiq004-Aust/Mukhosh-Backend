5d846e65e65e03f13b7b6d8431e4a7d579b989c9
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-10 16:32:47 +0600

    Update UserController.cs
    Async and await

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -9,6 +9,7 @@ using api.DTOs.User;
 using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 //Controller is a class that handles the HTTP requests and responses
 //Also it directly interacts with the database through the ApplicationDBContext class, which is injected into the controller through dependency injection.
@@ -25,20 +26,21 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAllUsers()
+        public async Task<IActionResult> GetAllUsers()
         {
-            var users = _context.User.ToList().Select(user => user.ToUserInView());
+            var users = await _context.User.ToListAsync(); // only async part is the database call, not the mapping
+            var usersInView = users.Select(user => user.ToUserInView());
             //map the user model to the response model using the mapper class
             //Select is used to project each user object to a UserInView object using the ToUserInView extension method defined in the UserMapper class.
 
-            return Ok(users); //200 OK status code with the list of users in the response body
+            return Ok(usersInView); //200 OK status code with the list of users in the response body
         }
 
         [HttpGet]
         [Route("{id:guid}")] // api/User/{id}
-        public IActionResult GetUserById([FromRoute] Guid id)
+        public async Task<IActionResult> GetUserById([FromRoute] Guid id)
         {
-            var user = _context.User.Find(id); // FirstOrDefault also works, but Find is more efficient because it uses the primary key to find the user.
+            var user = await _context.User.FindAsync(id); // FirstOrDefault also works, but Find is more efficient because it uses the primary key to find the user.
             if (user == null)
             {
                 return NotFound();//404 Not Found status code if the user is not found
@@ -49,11 +51,11 @@ namespace api.Controllers
         [HttpPost]
         //FromBody is used to bind the request body to the user object
         //Taking request body object and mapping it to the User model using the mapper class, then saving it to the database.
-        public IActionResult CreateUser([FromBody] UserInCreate userInCreateObject)
+        public async Task<IActionResult> CreateUser([FromBody] UserInCreate userInCreateObject)
         {
             var user = userInCreateObject.ToUserInCreate(); //map the request body to the User model using the mapper class
-            _context.User.Add(user); // add the user to the database context
-            _context.SaveChanges(); // save the changes to the database
+            await _context.User.AddAsync(user); // add the user to the database context
+            await _context.SaveChangesAsync(); // save the changes to the database
 
             //201 Created status code with the user in the response body
             return CreatedAtAction(nameof(GetUserById), new { id = user.UserId }, user.ToUserInView());
@@ -62,9 +64,9 @@ namespace api.Controllers
 
         [HttpPut]
         [Route("{id:guid}")]
-        public IActionResult UpdateUser([FromRoute] Guid id, [FromBody] UserInUpdate userInUpdateObject)
+        public async Task<IActionResult> UpdateUser([FromRoute] Guid id, [FromBody] UserInUpdate userInUpdateObject)
         {
-            var user = _context.User.Find(id);
+            var user = await _context.User.FindAsync(id);
             if (user == null)
             {
                 return NotFound();
@@ -74,23 +76,25 @@ namespace api.Controllers
             user.Name = updatedUser.Name;
             user.Phone = updatedUser.Phone;
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(user.ToUserInView());
         }
         
 
         [HttpDelete]
         [Route("{id:guid}")]
-        public IActionResult DeleteUser([FromRoute] Guid id)
+        public async Task<IActionResult> DeleteUser([FromRoute] Guid id)
         {
-            var user = _context.User.Find(id);
+            var user = await _context.User.FindAsync(id);
             if (user == null)
             {
                 return NotFound();
             }
 
             _context.User.Remove(user);
-            _context.SaveChanges();
+            // remove is not async, because it only marks the entity for deletion, and does not actually delete it from the database until SaveChangesAsync is called.
+            await _context.SaveChangesAsync();
+            
             return NoContent(); //204 No Content status code, because the user has been deleted and there is no content to return
         }
 
