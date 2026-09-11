5b76f843183337fd5a8c75a791feeb34243f247a
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-11 22:19:47 +0600

    Migrations

---
+++ api/Migrations/20260911160844_AddVibeToPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVibeToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Vibe",
+                table: "Post",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Vibe",
+                table: "Post");
+        }
+    }
+}
