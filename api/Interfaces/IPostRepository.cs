3f66298c273170507022b3902507ea1b0ce16ea8
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-11 22:20:18 +0600

    Update IPostRepository.cs

--- api/Interfaces/IPostRepository.cs
+++ api/Interfaces/IPostRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.DTOs.Post;
+using api.Helper;
 using api.Models;
 
 namespace api.Interfaces
 {
     public interface IPostRepository
     {
-        Task<List<Post>> GetAllPostsAsync();
+        Task<List<Post>> GetAllPostsAsync(QueryObject query);
         Task<Post?> GetPostByIdAsync(Guid postId);
         Task<Post> CreatePostAsync(PostInCreate post);
         Task<Post?> UpdatePostAsync(Guid postId, PostInUpdate post);
