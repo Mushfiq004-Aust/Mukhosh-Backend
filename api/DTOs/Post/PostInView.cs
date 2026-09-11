55f1dcdad87831758cf640321ffcd7053e4f26bc
Mushfiqur Rahman Sajid <plove1691@gmail.com>
2026-09-11 15:58:03 +0600

    Update PostInView.cs

--- api/DTOs/Post/PostInView.cs
+++ api/DTOs/Post/PostInView.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.DTOs.Comment;
 
 namespace api.DTOs.Post
 {
@@ -10,7 +11,8 @@ namespace api.DTOs.Post
         public required string Title { get; set; }
         public required string Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-
+        
         //add comments too
+        public List<CommentInView> Comments { get; set; }
     }
 }
